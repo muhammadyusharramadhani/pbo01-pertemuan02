a6153cc1bc717dbbe01cb30d1e7ef7432cad25ae
muhammadyusharramadhani <62274391+muhammadyusharramadhani@users.noreply.github.com>
2020-03-26 18:26:16 +0800

    Buat Output

--- InputOutput.java
+++ InputOutput.java
@@ -7,14 +7,14 @@ public class InputOutput{
   int jumlahSks;
   double IPK;
 
-  System.out.println("Masukkan nama anda: ");
+  System.out.print("Masukkan nama anda: ");
   nama = scanner.nextLine();
   System.out.println("Hai, "+nama);
 
-  System.out.println("Masukkan jumlah SKS yang sudah ditempuh: ");
+  System.out.print("Masukkan jumlah SKS yang sudah ditempuh: ");
   jumlahSks = scanner.nextInt();
   
-  System.out.println("Masukkan IPK Sementara : ");
+  System.out.print("Masukkan IPK Sementara :");
   IPK = scanner.nextDouble();
 
   double jumlah = IPK * jumlahSks;
